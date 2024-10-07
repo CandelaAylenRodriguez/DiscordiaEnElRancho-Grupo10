06f62aaef4facd2ea75b75ccc7f6c3fcde78022f
agustinsala1996 <agustinsala1996@gmail.com>
2024-10-07 01:59:34 -0300

    feature/puntaje funcionando

---
+++ src/components/PuntajeComponent.js
@@ -0,0 +1,30 @@
+export class PuntajeComponent {
+    constructor(scene, puntajeInicial = 0) {
+        this.scene = scene;
+        this.puntaje = puntajeInicial; // Inicializa el puntaje con el valor recibido
+
+        // Crea un texto en la esquina superior derecha para mostrar el puntaje
+        this.puntajeText = this.scene.add.text(
+            this.scene.cameras.main.width - 300, // Posición en X
+            16, // Posición en Y
+            `Puntaje: ${this.puntaje}`, // Texto inicial
+            {
+                fontFamily: 'Arial Black',
+                fontSize: 38,
+                color: '#00FA9A',
+                stroke: '#20B2AA',
+                strokeThickness: 8,
+            }
+        );
+
+        // Agrega el puntaje al registro para poder acceder a él más tarde
+        this.scene.registry.set('puntaje', this.puntaje);
+    }
+
+    // Método para aumentar el puntaje
+    aumentarPuntaje(cantidad) {
+        this.puntaje += cantidad; // Incrementa el puntaje
+        this.puntajeText.setText(`Puntaje: ${this.puntaje}`); // Actualiza el texto mostrado
+        this.scene.registry.set('puntaje', this.puntaje); // Actualiza el registro
+    }
+}
