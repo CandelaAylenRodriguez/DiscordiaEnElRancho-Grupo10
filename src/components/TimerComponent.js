b8c477eeca9af891c49a3c46d8cd022cc34b7aa2
CandelaAylenRodriguez <candelarodriguezouo@gmail.com>
2024-10-28 10:22:19 -0300

    Se actualizaron los assets de los personajes.

--- src/components/TimerComponent.js
+++ src/components/TimerComponent.js
@@ -3,7 +3,7 @@ import { getPhrase } from '../services/translations'; // Asegúrate de que la ru
 export class TimerComponent {
   constructor(scene, onComplete) {
     this.scene = scene;
-    this.initialTime = 15; // Tiempo inicial en segundos
+    this.initialTime =  60// Tiempo inicial en segundos
     this.previousTime = this.scene.registry.get('timer'); // Obtener el tiempo anterior del registro
     const uiScene = this.scene.scene.get('UI');  // Acceder a la escena 'UI'
     // Si no existe, inicializa en 120, de lo contrario, suma 30
