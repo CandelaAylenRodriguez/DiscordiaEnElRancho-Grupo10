74a4a097c88a1d7ad115fb5b3affad28c00dfc04
agustinsala1996 <agustinsala1996@gmail.com>
2024-10-30 23:25:13 -0300

    Fix

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -10,6 +10,12 @@ export class GameOver extends Scene {
         this.cameras.main.setBackgroundColor(0x343434);
 
         this.add.image(960, 540, 'boton').setAlpha(0.5).setScale(2);
+        // Detener todos los sonidos al iniciar la escena Victoria
+        this.sound.stopAll();
+
+        // Iniciar la música específica de la escena Victoria
+        this.MusicaGameOver = this.sound.add('MusicGameOver', { loop: false });
+        this.MusicaGameOver.play();
 
         this.add.text(960, 540, getPhrase('PERDIERON'), {
             fontFamily: 'SuperBrain',
@@ -20,7 +26,11 @@ export class GameOver extends Scene {
             align: 'center'
         }).setOrigin(0.5);
 
+        // Volver al menú principal al hacer clic, y detener la música de Victoria
         this.input.once('pointerdown', () => {
+            if (this.MusicaGameOver) {
+                this.MusicaGameOver.stop();
+            }
             this.scene.start('MainMenu');
         });
     }
